e172b3dd0d2d5a1f137aed4498f3a602afd5b60e
Kamal Fariz Mahyuddin <kamal@zoecity.com>
2009-05-04 00:06:58 +0800

    Update the footer with license and github repo

--- wp-content/themes/geekcampkl/footer.php
+++ wp-content/themes/geekcampkl/footer.php
@@ -1,6 +1,6 @@
     <div class="clear"></div>
   </div>
-  <div id="footer"><span>&copy;2009 <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a>. Some Rights Reserved.</span><span>Comissioned by <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a> volunteers!</span></div>
+  <div id="footer"><span>Licensed under the <a href="http://sam.zoy.org/wtfpl/">WTFPL</a>. All code on <a href="http://github.com/kamal/geekcamp">GitHub</a>.</span><span>Built by <a href="<?php echo get_option('home'); ?>/"><?php bloginfo('name'); ?></a> volunteers!</span></div>
 </div>
 </body>
 </html>

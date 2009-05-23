43bf6466e6abc1b6c420a8d33615417c1a02a896
Ariff Azraai <mk9206.nx@gmail.com>
2009-05-23 22:57:26 +0800

    single.php loop bugs

--- wp-content/themes/geekcampkl/single.php
+++ wp-content/themes/geekcampkl/single.php
@@ -11,11 +11,21 @@
         <p class="meta tags"><?php the_tags('Tags: ', ', ', '<br />'); ?></p>
         <p class="meta category comment">Posted in <?php the_category(', ') ?> | <?php edit_post_link('Edit', '', ' | '); ?>  <?php comments_popup_link('No Comments &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
       </div>
+      <?php endwhile; ?>
       <div class="pagination">
         <div class="left"><?php previous_post_link('&larr; %link') ?></div>
         <div class="right"><?php next_post_link('%link &rarr;') ?></div>
         <div class="clear"></div>
       </div>
+      <?php else: ?>
+      <h1 class="page_title">Oops! We Kenot Fain  Your Fail!</h1>
+      <p class="description">Somehow, the file is missing!</p>
+      <span class="home right"><a href="/">Back To Homepage</a></span>
+      <div id="404">
+      <p>Try to search what you are looking for</p>
+      <?php get_search_form(); ?>
+      </div>
+      <?php endif; ?>
     </div>
     <?php get_sidebar(); ?>
     <div class="clear"></div>
